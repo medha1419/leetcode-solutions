0ad74d5d49fe30146e28e98d929990c778e761c1
Medha Gupta <officialguptamedha@gmail.com>
2026-08-21 22:29:54 +0530

    Time: 2 ms (77.46%), Space: 8.2 MB (17.24%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,7 +1,13 @@
 class Solution {
 public:
-    int fib(int n) {
+    int helper(int n, vector<int>& memo){
         if(n<=1) return n;
-        return fib(n-1)+fib(n-2);
+        if(memo[n]!=-1) return memo[n];
+        memo[n]=helper(n-1,memo)+ helper(n-2,memo);
+        return memo[n];
+    }
+    int fib(int n){
+        vector<int> memo(n+1,-1);
+        return helper(n,memo);
     }
 };
